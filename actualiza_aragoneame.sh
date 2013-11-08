468df5bae979d2235de5507b4855e9e1bde2609b
root <root@srv1.ibercivis.es>
2013-11-08 11:33:48 +0100

    Deleted file

--- actualiza_aragoneame.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-wget "http://www.aragoneame.com/blogliticos/wp-content/plugins/wp-o-matic/cron.php?code=90a155bf" -O - &> /dev/null
-if [ $? -ne 0 ]
-then
-	echo "ERROR actualizando aragonéame."
-	exit 1
-fi
-
